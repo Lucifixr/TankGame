d37830f7265632d1c3945a64d4182f70f901f477
Matthijs Nonkesgit config --global user.email mnonkes2@st.noorderpoort.nl <113519242+Lucifixr@users.noreply.github.com>
2022-11-15 13:52:49 +0100

    Tilesheet
    Heb een tilesheet in mijn game gegooid en heb mijn tank en enviroment veranderd

--- Assets/Prefab/ParticleDestr.cs
+++ Assets/Prefab/ParticleDestr.cs
@@ -22,4 +22,24 @@ public class ParticleDestr : MonoBehaviour
             Destroy(gameObject);
         }
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Raakt player2");
+
+            GameObject.Find("Canvas").GetComponent<Scorescript>().AddP2Score();
+        }
+        if (collision.gameObject.CompareTag("Player2"))
+        {
+            Debug.Log("Raakt Player1");
+
+            GameObject.Find("Canvas").GetComponent<Scorescript>().AddP1Score();
+        }
+    }
+
+
+
+
 }
